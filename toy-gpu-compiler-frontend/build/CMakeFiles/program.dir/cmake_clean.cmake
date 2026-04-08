2024773206ead0216f7cbdbc7e25c5c7177674b8
ryanashita <ryunosukeyanashita@gmail.com>
2026-04-08 02:25:27 +0000

    install cmake into project

---
+++ toy-gpu-compiler-frontend/build/CMakeFiles/program.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program.dir/parser.cpp.o"
+  "CMakeFiles/program.dir/parser.cpp.o.d"
+  "CMakeFiles/program.dir/program.cpp.o"
+  "CMakeFiles/program.dir/program.cpp.o.d"
+  "program"
+  "program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
